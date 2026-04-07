47f4e135383877cbc5469008a5815a7b49a080ad
Isira Hansaja <isirahansaja@gmail.com>
2026-04-07 13:24:10 +0530

    Added a temporary logout button to check

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:transist_tracker/providers/auth_provider.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends ConsumerWidget {
   const ProfilePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
       body: Center(
-        child: Text(
-          'profile Page',
-          style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'Profile Page',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                await ref.read(authProvider.notifier).logout();
+
+                if (context.mounted) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Logged out'),
+                    ),
+                  );
+                }
+              },
+              child: const Text('Logout'),
+            ),
+          ],
         ),
       ),
     );
